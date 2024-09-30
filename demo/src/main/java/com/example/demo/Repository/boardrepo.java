66704b9b02b6ca71d4583a7914bc50dbe0b4443d
dheeraj052000 <dheerajkandagatla912@gmail.com>
2024-09-30 18:27:33 -0500

    30 th date

--- demo/src/main/java/com/example/demo/Repository/boardrepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Models.board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface boardrepo extends JpaRepository<board, Integer> {
-}
